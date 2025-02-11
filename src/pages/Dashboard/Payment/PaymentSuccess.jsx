3b43b92b09793a56654ca30467738f97a44c34bd
mdmasudrana271 <mr7123619@gmail.com>
2025-02-11 18:24:41 +0600

    added payment success and failed page

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
+import animation from "../../../assets/animation/paymentsucc.json";
+
 const PaymentSuccess = () => {
   return (
-    <div>
-      <h1>your payment is successfull</h1>
+    <div className="flex items-center justify-center min-h-screen bg-green-50">
+      <div className="bg-white shadow-md rounded-lg p-8 max-w-md text-center border border-green-300">
+        <div className="flex justify-center">
+          <figure className="h-full md:w-80">
+            <Lottie animationData={animation} loop={true} />
+          </figure>
+        </div>
+
+        <h1 className="text-3xl font-bold text-green-700 mt-4">
+          Payment Successful!
+        </h1>
+        <p className="text-gray-600 mt-2">
+          Thank you for your payment! <br /> Your transaction was completed
+          smoothly. <br />
+          <span className="text-green-600 font-semibold"> Eco Greens</span>{" "}
+          <br />
+          appreciates your support in making the world a greener place! 🌱
+        </p>
+
+        {/* Button to Homepage */}
+        <Link
+          to="/"
+          className="mt-6 inline-block bg-green-600 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-green-700 transition"
+        >
+          Back to Homepage
+        </Link>
+      </div>
     </div>
   );
 };
